4cb67eced0b618715337333f02bade793e110654
Aztec331 <adityabuzz331@gmail.com>
2026-01-23 09:42:55 +0530

    Finish the game

--- src/App.tsx
+++ src/App.tsx
@@ -11,25 +11,6 @@ export default function App(){
     const { width, height } = useWindowSize();
 
     const buttonRef = useRef<HTMLButtonElement>(null);
-    console.log(buttonRef);
-
-    /**
-    * Challenge:
-    * Make it so when the game is over, the "New Game" button
-    * automatically receives keyboard focus so keyboard users
-    * can easily trigger that button without having to tab
-    * through all the dice first.
-    * 
-    * Hints:
-    * 1. Focusing a DOM element with the DOMNode.focus() method
-    *    requires accessing the native DOM node. What tool have
-    *    we learned about that allows us to do that?
-    * 
-    * 2. Automatically calling the .focus() on a DOM element when
-    *    the game is won requires us to synchronize the local
-    *    `gameWon` variable with an external system (the DOM). What
-    *    tool have we learned about that allows us to do that?
-    */
 
     //Check if all dice's isHeld is true and have the same value
     //store the boolean result in gameWon
@@ -48,6 +29,7 @@ export default function App(){
 
     //if condition means what to do when gameWon changes
     //dpendency array means when to run the effect
+    //dependency array has gameWon so it runs whenever gameWon changes
     useEffect( () => {
 
     if(gameWon===true && buttonRef.current){
@@ -57,28 +39,17 @@ export default function App(){
     },[gameWon])
 
     // //Generates an array of 10 random dice values
-    // function generateAllNewDice(): DiceSetType[]{
-    //     const diceSet: DiceSetType[] = []
-
-    // for(let i = 0; i < 10; i++){
-    // diceSet.push({value: Math.floor(Math.random() * 6) + 1, 
-    //     isHeld: false, id: nanoid()})
-    // }
-
-    //     return diceSet
-    // }
-
-    //Testing function to generate fixed dice values
     function generateAllNewDice(): DiceSetType[]{
         const diceSet: DiceSetType[] = []
 
     for(let i = 0; i < 10; i++){
-    diceSet.push({value: 9, 
+    diceSet.push({value: Math.floor(Math.random() * 6) + 1, 
         isHeld: false, id: nanoid()})
     }
 
         return diceSet
     }
+
 
     //dice = [1,2,3,4,5,6,1,2,3,4]  // Example array for testing
     //obj means each object in the array
@@ -137,6 +108,7 @@ export default function App(){
         <button className="Roll_button" onClick={rollDice} ref={buttonRef}>{gameWon ? "New Game" : "Roll"}</button>
 
         </main>
+
 
     )
 }
